8905bbd2c93ce684b53a3aaf4089c565cb65dc25
Kun Chen <kunchenguid@gmail.com>
2021-12-22 12:20:06 -0800

    test adding and removing components

--- test/entity.test.ts
+++ test/entity.test.ts
@@ -1,4 +1,8 @@
-import { testEngine } from './testComponentTypes';
+import {
+  TestComponentType1,
+  TestComponentType2,
+  testEngine,
+} from './testComponentTypes';
 
 describe('entity', () => {
   it('should create and destroy correctly', () => {
@@ -12,5 +16,34 @@ describe('entity', () => {
     world.destroyEntity(entityId);
 
     expect(world.hasEntity(entityId)).toBeFalsy();
+  });
+
+  it('should add and remove components correctly', () => {
+    const world = testEngine.createWorld();
+
+    const entityId = world.createEntity();
+
+    const component1 = world.addComponentToEntity(entityId, TestComponentType1);
+    const component1FromGet = world.getComponentFromEntity(
+      entityId,
+      TestComponentType1
+    );
+    expect(component1FromGet).toEqual(component1);
+
+    const nonExistent = world.getComponentFromEntity(
+      entityId,
+      TestComponentType2
+    );
+    expect(nonExistent).toBeNull();
+
+    world.addComponentToEntity(entityId, TestComponentType2);
+    const component2 = world.addComponentToEntity(entityId, TestComponentType2);
+    expect(component2).not.toBeFalsy();
+
+    const allComponents = world.getComponentsFromEntity(entityId);
+    expect(allComponents).toHaveLength(2);
+
+    expect(allComponents).toContain(component1);
+    expect(allComponents).toContain(component2);
   });
 });
